f79950bb01ffbb317d99e62db84c8522f4913e8a
skulkarni207 <skulkarni207@IN-4H3K9S3>
2024-02-14 09:33:04 +0530

    Add project files.

---
+++ LoginScreenAss/LoginScreenAss/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LoginScreenAss
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		private void Button_Clicked(object sender, EventArgs e)
+		{
+			string un = username.Text;
+			string pw = password.Text;
+
+			if (un == "admin" && pw == "admin")
+			{
+				DisplayAlert("Login Success", "You have successfully logged in.", "OK");
+			}
+			else
+			{
+				DisplayAlert("Login Failed", "Invalid username or password. Please try again.", "OK");
+			}
+
+		}
+    }
+}
